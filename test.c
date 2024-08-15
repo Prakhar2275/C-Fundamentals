309289bf4531fd6df8ed9ce9ce7386f2ae8da884
Prakhar@2275 <prakharchauhan24853@gmail.com>
2024-08-15 09:33:29 +0530

    question 1

--- test.c
+++ test.c
@@ -1,47 +1,42 @@
-#include <stdio.h>
-
-void shiftLeft(int arr[], int size) {
-    // Save the first element to be placed at the end
-    int first = arr[0];
-    
-    // Shift all elements to the left by one position
-    for (int i = 0; i < size - 1; i++) {
-        arr[i] = arr[i + 1];
+#include<stdio.h>
+void bubsort(int arr[],int n){
+    int c;
+    int swapped;
+    for(int i=0;i<n-1;i++){
+        swapped=0;
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                c=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=c;
+                swapped=1;
+            }
+        }
+        if(swapped==0){
+            break;
+        }
     }
-    
-    // Place the saved first element at the end
-    arr[size - 1] = first;
-}
-
-int main() {
-    int size;
-
-    // Input size of the array
-    printf("Enter the number of elements in the array: ");
-    scanf("%d", &size);
-
-    if (size <= 0) {
-        printf("Array size must be greater than 0.\n");
-        return 1;
-    }
-
-    int arr[size];
-
-    // Input elements of the array
-    printf("Enter the elements of the array:\n");
-    for (int i = 0; i < size; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    // Perform left shift
-    shiftLeft(arr, size);
-
-    // Output the shifted array
-    printf("Shifted array:\n");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
+    for(int i=0;i<n;i++){
+        printf("%d  ",arr[i]);
     }
     printf("\n");
-
+}
+int main(){
+    int a;
+    printf("Enter the size of the array: ");
+    scanf("%d",&a);
+    int arr[a];
+    printf("Enter %d elements in the array: ");
+    for(int i=0;i<a;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("\n");
+    printf("Elemnets enetered by the user in the array: \n");
+    for(int i=0;i<a;i++){
+        printf("%d  ",arr[i]);
+    }
+    printf("\n");
+    printf("Elements of the arrayy after getting sorteed is: \n");
+    bubsort(arr,a);
     return 0;
 }
