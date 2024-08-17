100325657b296dbb3422a05b8712e543e096d68d
Prakhar@2275 <prakharchauhan24853@gmail.com>
2024-08-17 18:49:03 +0530

    test changes

--- sample2.c
+++ sample2.c
@@ -1,42 +1,42 @@
 #include<stdio.h>
-void bubsort(int arr[],int n){
-    int c;
-    int swapped;
-    for(int i=0;i<n-1;i++){
-        swapped=0;
-        for(int j=0;j<n-1-i;j++){
-            if(arr[j]>arr[j+1]){
-                c=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=c;
-                swapped=1;
-            }
-        }
-        if(swapped==0){
-            break;
-        }
+#include<math.h>
+
+int count(int n){
+    int r;
+    int c=0;
+    while(n!=0){
+        r=n%10;
+        c++;
+        n/=10;
     }
+    return (c);
 }
+
+int arms(int n){
+    int ogno=n;
+    int r,sum=0;
+    int c=count(n);
+    while(n!=0){
+        r=n%10;
+        sum+=pow(r,c);
+        n/=10;
+    }
+    return (ogno==sum);
+}
+
 int main(){
     int a;
-    printf("Enter the size of the array: ");
+
+    printf("Enter a no. to check it is armstrong or not: ");
     scanf("%d",&a);
-    printf("Enter %d charachters in the array: \n",a);
-    int arr[a];
-    for(int i=0;i<a;i++){
-        scanf("%d",&arr[i]);
+
+    int i=arms(a);
+
+    if(i==0){
+        printf("It is not an armstrong no. !!!");
     }
-    printf("\n");
-    printf("Entered array by the user is \n");
-    for(int i=0;i<a;i++){
-        printf("%d  ",arr[i]);
+    else if(i==1){
+        printf("It is an armstrong no.!!!!!");
     }
-    printf("\n");
-    bubsort(arr,a);
-    printf("Sorted array: \n");
-    for(int i=0;i<a;i++){
-        printf("%d  ",arr[i]);
-    }
-    printf("\n");
     return 0;
 }
