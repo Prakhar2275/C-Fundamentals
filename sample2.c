905c92163faf26053e0af00ab45f42ae31420432
Prakhar@2275 <prakharchauhan24853@gmail.com>
2024-08-17 18:43:06 +0530

    test changes

--- sample2.c
+++ sample2.c
@@ -1,34 +1,42 @@
-#include<string.h>
 #include<stdio.h>
-void bubsort(char str[]);
-void bubsort(char str[]){
-  char temp;
-  int swapped;
-  int n=strlen(str);
-  for(int i=0;i<n-1;i++){
-    swapped=0; 
-    for(int j=0;j<n-i-1;j++){
-      if(str[j]>str[j+1]){
-        temp=str[j];
-        str[j]=str[j+1];
-        str[j+1]=temp;
-        swapped=1;
-      }
+void bubsort(int arr[],int n){
+    int c;
+    int swapped;
+    for(int i=0;i<n-1;i++){
+        swapped=0;
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                c=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=c;
+                swapped=1;
+            }
+        }
+        if(swapped==0){
+            break;
+        }
     }
-    if(swapped==0){
-      break;
-    }
-  }
 }
-  
-
-
 int main(){
-  char str[100];
-  printf("Enter the string to sort it : ");
-  fgets(str,sizeof(str),stdin);
-  str[strcspn(str,"\n")]="\0";
-  bubsort(str);
-  printf("Sorted string is: %s \n",str);
-  return 0;
+    int a;
+    printf("Enter the size of the array: ");
+    scanf("%d",&a);
+    printf("Enter %d charachters in the array: \n",a);
+    int arr[a];
+    for(int i=0;i<a;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("\n");
+    printf("Entered array by the user is \n");
+    for(int i=0;i<a;i++){
+        printf("%d  ",arr[i]);
+    }
+    printf("\n");
+    bubsort(arr,a);
+    printf("Sorted array: \n");
+    for(int i=0;i<a;i++){
+        printf("%d  ",arr[i]);
+    }
+    printf("\n");
+    return 0;
 }
--- test.c
+++ test.c
@@ -1,32 +1,117 @@
-#include<string.h>
-#include<stdio.h>
-void bubusort(char str[]);
-int main(){
-    char str[100];
-    printf("Enter a string: ");
-    fgets(str,sizeof(str),stdin);
-    str[strcspn(str,"\n")]="\0";
-    bubsort(str);
-    printf("Sorted string is %s",str);
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+
+// Function to evaluate the expression
+double evaluate(const char *expression);
+
+// Helper function to get the precedence of operators
+int precedence(char op) {
+    switch (op) {
+        case '+': case '-': return 1;
+        case '*': case '/': return 2;
+        case '^': return 3;
+        default: return 0;
+    }
 }
-void bubsort(char str[]){
-    int n=strlen(str);
-    int i,j;
-    char temp;
-    int swapped;
-    for(int i=0;i<n-1;i++){
-        swapped=0;
-        for(int j=0;j<n-1-i;j++){
-            if(str[j]>str[j+1]){
-                temp=str[j];
-                str[j]=str[j+1];
-                str[j+1]=temp;
-                swapped=1;
-            }
+
+// Function to perform arithmetic operations
+double applyOp(double a, double b, char op) {
+    switch (op) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+        case '^': return pow(a, b);
+        default: return 0;
+    }
+}
+
+// Function to evaluate a simple expression without parentheses
+double evaluateSimple(const char *expression) {
+    double values[100];
+    char ops[100];
+    int valuesTop = -1;
+    int opsTop = -1;
+
+    for (int i = 0; expression[i]; i++) {
+        if (isspace(expression[i])) {
+            continue;
         }
-        if(swapped==0){
-            break;
+
+        if (isdigit(expression[i])) {
+            double value = 0;
+            while (i < strlen(expression) && isdigit(expression[i])) {
+                value = value * 10 + (expression[i] - '0');
+                i++;
+            }
+            i--;
+            values[++valuesTop] = value;
+        } else if (expression[i] == '(') {
+            ops[++opsTop] = expression[i];
+        } else if (expression[i] == ')') {
+            while (opsTop != -1 && ops[opsTop] != '(') {
+                double val2 = values[valuesTop--];
+                double val1 = values[valuesTop--];
+                char op = ops[opsTop--];
+                values[++valuesTop] = applyOp(val1, val2, op);
+            }
+            opsTop--; // Remove '('
+        } else {
+            while (opsTop != -1 && precedence(ops[opsTop]) >= precedence(expression[i])) {
+                double val2 = values[valuesTop--];
+                double val1 = values[valuesTop--];
+                char op = ops[opsTop--];
+                values[++valuesTop] = applyOp(val1, val2, op);
+            }
+            ops[++opsTop] = expression[i];
         }
     }
+
+    while (opsTop != -1) {
+        double val2 = values[valuesTop--];
+        double val1 = values[valuesTop--];
+        char op = ops[opsTop--];
+        values[++valuesTop] = applyOp(val1, val2, op);
+    }
+
+    return values[valuesTop];
+}
+
+// Function to remove trailing '=' sign and evaluate the expression
+double evaluate(const char *expression) {
+    size_t length = strlen(expression);
+    if (expression[length - 1] == '=') {
+        char *expr = malloc(length);
+        strncpy(expr, expression, length - 1);
+        expr[length - 1] = '\0';
+        double result = evaluateSimple(expr);
+        free(expr);
+        return result;
+    } else {
+        return evaluateSimple(expression);
+    }
+}
+
+int main() {
+    char expression[256];
+    int numExpressions;
+
+    printf("Enter the number of expressions: ");
+    scanf("%d", &numExpressions);
+    getchar();  // Consume the newline character after the number of expressions
+
+    for (int i = 0; i < numExpressions; i++) {
+        printf("Enter expression %d: ", i + 1);
+        fgets(expression, sizeof(expression), stdin);
+        // Remove the newline character from fgets
+        expression[strcspn(expression, "\n")] = '\0';
+
+        double result = evaluate(expression);
+        printf("Result: %f\n", result);
+    }
+
+    return 0;
 }
