49162c0bc169d338c1df6e3b146fde7bc2699649
Prakhar@2275 <prakharchauhan24853@gmail.com>
2024-08-17 18:55:58 +0530

    test chaanges

--- sample2.c
+++ sample2.c
@@ -1,42 +1,34 @@
+//sorting of a sting iin c 
 #include<stdio.h>
-#include<math.h>
+#include<string.h>
 
-int count(int n){
-    int r;
-    int c=0;
-    while(n!=0){
-        r=n%10;
-        c++;
-        n/=10;
+void bubsort(char str[]){
+    int n=strlen(str);
+    int i,j;
+    char temp;
+    int swapped;
+    for(int i=0;i<n-1;i++){
+        swapped=0;
+        for(int j=0;j<n-1-i;j++){
+            if(str[j]>str[j+1]){
+                temp=str[j];
+                str[j]=str[j+1];
+                str[j+1]=temp;
+                swapped=1;
+            }
+        }
+        if(swapped==0){
+            break;
+        }
     }
-    return (c);
+
 }
-
-int arms(int n){
-    int ogno=n;
-    int r,sum=0;
-    int c=count(n);
-    while(n!=0){
-        r=n%10;
-        sum+=pow(r,c);
-        n/=10;
-    }
-    return (ogno==sum);
-}
-
-int main(){
-    int a;
-
-    printf("Enter a no. to check it is armstrong or not: ");
-    scanf("%d",&a);
-
-    int i=arms(a);
-
-    if(i==0){
-        printf("It is not an armstrong no. !!!");
-    }
-    else if(i==1){
-        printf("It is an armstrong no.!!!!!");
-    }
+ int main(){
+    char str[100];
+    printf("Enter the string to sort it:  ");
+    fgets(str,sizeof(str),stdin);
+    str[strcspn(str,"\n")]="\0";
+    bubsort(str);
+    printf("Sorted string: %s",str);
     return 0;
-}
+ }
