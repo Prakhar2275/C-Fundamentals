a112793a4f178aae5cf4aad77562d71cb3f6f919
Prakhar@2275 <prakharchauhan24853@gmail.com>
2024-08-18 09:21:16 +0530

    q2

--- sample2.c
+++ sample2.c
@@ -1,17 +1,18 @@
-//Write a C program to compute the sum of the two input values. If the two values are the same, then return triple their sum.
+//Write a C program that will take a number as input and find the absolute difference between the input number and 51. If the input number is greater than 51, it will return triple the absolute difference.
 #include<stdio.h>
 
 int main(){
-    int a,b;
-    printf("Enter the first no. : ");
+    int a;
+    printf("Enter a no. :");//taking input of a no.
     scanf("%d",&a);
-    printf("Enter the seecond no. :");
-    scanf("%d",&b);
-    if(a==b){
-        printf("Sum of both the no. and tripleng it gives:%d \n",a*6);
+    if(a<51){
+        printf("Diffrence between 51 and %d is %d \n",a,51-a);
     }
-    int sum;
-    sum=a+b;
-    printf("Sum of both the no. is %d",sum);
+    else if(51<a){
+        printf("Triple of absolute diffrence is %d",((a-51)*3));
+    }
+    else{
+        printf("Diffrence is zero ad the input is 51 \n");
+    }
     return 0;
 }
