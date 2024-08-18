93d2ce5fcbd37c76f622e00957b20e3d94077ff0
Prakhar@2275 <prakharchauhan24853@gmail.com>
2024-08-18 09:16:04 +0530

    q1

--- sample2.c
+++ sample2.c
@@ -1,34 +1,17 @@
-//sorting of a sting iin c 
+//Write a C program to compute the sum of the two input values. If the two values are the same, then return triple their sum.
 #include<stdio.h>
-#include<string.h>
 
-void bubsort(char str[]){
-    int n=strlen(str);
-    int i,j;
-    char temp;
-    int swapped;
-    for(int i=0;i<n-1;i++){
-        swapped=0;
-        for(int j=0;j<n-1-i;j++){
-            if(str[j]>str[j+1]){
-                temp=str[j];
-                str[j]=str[j+1];
-                str[j+1]=temp;
-                swapped=1;
-            }
-        }
-        if(swapped==0){
-            break;
-        }
+int main(){
+    int a,b;
+    printf("Enter the first no. : ");
+    scanf("%d",&a);
+    printf("Enter the seecond no. :");
+    scanf("%d",&b);
+    if(a==b){
+        printf("Sum of both the no. and tripleng it gives:%d \n",a*6);
     }
-
-}
- int main(){
-    char str[100];
-    printf("Enter the string to sort it:  ");
-    fgets(str,sizeof(str),stdin);
-    str[strcspn(str,"\n")]="\0";
-    bubsort(str);
-    printf("Sorted string: %s",str);
+    int sum;
+    sum=a+b;
+    printf("Sum of both the no. is %d",sum);
     return 0;
- }
+}
