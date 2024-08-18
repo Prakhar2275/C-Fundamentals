17824209f77da8c1f304204116bd1a3458a35b61
Prakhar@2275 <prakharchauhan24853@gmail.com>
2024-08-18 09:26:35 +0530

    q3

--- sample2.c
+++ sample2.c
@@ -1,18 +1,16 @@
-//Write a C program that will take a number as input and find the absolute difference between the input number and 51. If the input number is greater than 51, it will return triple the absolute difference.
 #include<stdio.h>
 
 int main(){
-    int a;
-    printf("Enter a no. :");//taking input of a no.
+    int a,b;
+    printf("Enter the first no. :");
     scanf("%d",&a);
-    if(a<51){
-        printf("Diffrence between 51 and %d is %d \n",a,51-a);
-    }
-    else if(51<a){
-        printf("Triple of absolute diffrence is %d",((a-51)*3));
+    printf("Enter the second no. :");//input the characters
+    scanf("%d",&b);
+    if(a==30 || b==30 || a+b==30){//checking the condition
+        printf("True!!!!! \n");
     }
     else{
-        printf("Diffrence is zero ad the input is 51 \n");
+        printf("False!!!!!\n");
     }
     return 0;
 }
