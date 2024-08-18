3c0f6fc90a00d3dcd715f49c6965ab8b170f8577
Prakhar@2275 <prakharchauhan24853@gmail.com>
2024-08-18 10:26:11 +0530

    q5

--- sample2.c
+++ sample2.c
@@ -1,14 +1,25 @@
+//Write a C program to check whether the sequence of numbers 1, 2, 3 appears in a given array of integers somewhere.
 #include<stdio.h>
-
 int main(){
     int a;
-    printf("Enter the no :");
+    printf("Ehter the size of the array: ");
     scanf("%d",&a);
-    if(100<a<200){
-        printf("True!!!!");
+    int arr[a];
+    printf("Enter %d elements in the array: \n");
+    for(int i=0;i<a;i++){
+        scanf("%d",&arr[i]);
     }
-    else{
-        printf("False!!!!");
+    printf("Entered array by the user: \n");
+    for(int i=0;i<a;i++){
+        printf("%d  ",arr[i]);
     }
+    
+    for(int j=0;j<a-3;j++){
+        if(arr[j]==1 && arr[j+1]==2 && arr[j+2]){
+            printf("True!!!!!");
+        }
+    }
+
     return 0;
+    
 }
