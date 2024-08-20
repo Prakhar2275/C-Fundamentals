d9f116878a2df337b427d66d87cbe286b7c2c7ec
Prakhar@2275 <prakharchauhan24853@gmail.com>
2024-08-20 18:43:36 +0530

    question

--- sample2.c
+++ sample2.c
@@ -1,28 +1,45 @@
 #include<stdio.h>
+
+int triple(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        if(arr[i]==arr[i+1] && arr[i]==arr[i+2]){
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int main(){
     int a;
-    printf("Enter the size of the array: ");
+    printf("Enter the size of the array:");
     scanf("%d",&a);
     int arr[a];
-    printf("Enter %d characters in the array: ",a);
+    printf("Enter %d characters in the array: ");
     for(int i=0;i<a;i++){
         scanf("%d",&arr[i]);
     }
-    printf("\n");
-    printf("Elements entered in the arrayy: \n");
+    printf("Array Entered by the user is: \n");
     for(int i=0;i<a;i++){
         printf("%d  ",arr[i]);
     }
     printf("\n");
-    int i;
-    for(i=0;i<a-3;i++){
-        if(arr[i]==1 && arr[i+1]==2 &&  arr[i+2]==3){
-            printf("Entered array have that provided seuence in it");
-            break;
-        }
+    int n=triple(arr,a);
+    if(n==0){
+        printf("There is no triplet in the array: \n");
     }
-    if(i==a-3){
-        printf("Given sequence not founded inside the array!!!");
+    else if(n==1){
+        printf("An Triple lies inside the array!!!!\n");
+    }
+    int sum=0;
+    for(int i=0;i<a;i++){
+        sum+=arr[i];
+    }
+    printf("Sum of all the characters inside the array is %d \n",sum);
+    printf("You can easily detect the tripplet inside the array then why are you bothering me !!!! \n");
+    for(int i=0;i<a;i++){
+        printf("%d  ",arr[i]);
     }
     return 0;
+
+
 }
--- test.c
+++ test.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h> // For abs function
 
-bool isWithin10Of100Or200(int number) {
-    // Check if the number is within 10 of 100 or 200
-    return (abs(number - 100) <= 10) || (abs(number - 200) <= 10);
+// Function to check if a triple is present in the array
+int hasTriple(int arr[], int size) {
+    // Traverse the array
+    for (int i = 0; i < size - 2; i++) {
+        // Check if the current element and the next two elements are the same
+        if (arr[i] == arr[i + 1] && arr[i] == arr[i + 2]) {
+            return 1; // Triple found
+        }
+    }
+    return 0; // No triple found
 }
 
 int main() {
-    int number;
-    
-    // Prompt user for input
-    printf("Enter an integer: ");
-    scanf("%d", &number);
-    
-    // Check and display result
-    if (isWithin10Of100Or200(number)) {
-        printf("True\n");
-    } else {
-        printf("False\n");
+    int n;
+
+    // Input size of the array
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    // Declare the array
+    int arr[n];
+
+    // Input the array elements
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    
+
+    // Check if there is a triple in the array
+    if (hasTriple(arr, n)) {
+        printf("A triple is present in the array.\n");
+    } else {
+        printf("No triple is present in the array.\n");
+    }
+
     return 0;
 }
