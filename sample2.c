a172c4a6523c75ac4d15e16f49c9068832cb8a76
Prakhar@2275 <prakharchauhan24853@gmail.com>
2024-08-21 09:40:16 +0530

    change4

--- sample2.c
+++ sample2.c
@@ -3,12 +3,14 @@ int main(){
     int a;
     printf("Enter the no. :");
     scanf("%d",&a);
-    if(a%13==0 || (a+1)%13==0){
-        printf("True!!!!");
-
+    if(a%3==0){
+        printf("True!!3");
+    }
+    else if(a%7==0){
+        printf("True!!7");
     }
     else{
-        printf("False!!");
+        printf("False");
     }
     return 0;
 }
