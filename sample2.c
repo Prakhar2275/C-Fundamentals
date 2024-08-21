8b3c20107cce22fc1608463072ac3b6edc5b816b
Prakhar@2275 <prakharchauhan24853@gmail.com>
2024-08-21 09:46:16 +0530

    change 5

--- sample2.c
+++ sample2.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
+
 int main(){
     int a;
-    printf("Enter the no. :");
+    printf("Enter the  first no. :");
     scanf("%d",&a);
-    if(a%3==0){
-        printf("True!!3");
+    int c=0;
+    for(int  i=2;i<=10;i++){
+        if(a%i==0){
+            printf("%d is multiple of %d \n",a,i);
+            c++;
+        }
     }
-    else if(a%7==0){
-        printf("True!!7");
+    if(c==0){
+        printf("not within the range of 2 to 10!!!");
     }
-    else{
-        printf("False");
+    else if(c>0){
+        printf("The given no. is within multiple of 2 to 10!!!");
     }
     return 0;
 }
