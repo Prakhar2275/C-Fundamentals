dd710cd695611a8e00b39450f39953e263c0efc5
Prakhar@2275 <prakharchauhan24853@gmail.com>
2024-08-21 09:33:51 +0530

    change 3

--- sample2.c
+++ sample2.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 int main(){
-    int a,b;
-    printf("Enter the first no.:");
+    int a;
+    printf("Enter the no. :");
     scanf("%d",&a);
-    printf("Enter the second nno. :");
-    scanf("%d",&b);
-    if(a==5 || b==5  || a+b==5  || a-b==5  ||  b-a==5){
-        printf("True!!!");
+    if(a%13==0 || (a+1)%13==0){
+        printf("True!!!!");
+
     }
     else{
-        printf("False!!!");
+        printf("False!!");
     }
     return 0;
 }
