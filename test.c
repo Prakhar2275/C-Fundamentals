12eeeff6379f9f87edfb1f1f76b150703b9ec6a6
Prakhar@2275 <prakharchauhan24853@gmail.com>
2025-07-26 16:21:03 +0530

    few basic changements

--- test.c
+++ test.c
@@ -1,14 +1,35 @@
-#include<stdio.h>
+# include<stdio.h>
+
+
+int bubsort(int arr[], int n){
+    int c;
+    int swapped;
+
+    for(int i=0;i<n-1;i++){
+        swapped=0;
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                c=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=c;
+                swapped=0;
+            }
+        }
+        if(swapped==0){
+            break;
+        }
+    }
+
+    printf("Array After Getting Sorted: \n");
+    for(int i=0;i<n;i++){
+        printf("%d  ",arr[i]);
+    }
+    
+}
+
+
 
 int main(){
-    int a,b,c;
-    printf("Enter the first no. :");
-    scanf("%d",&a);
-    printf("Enter the first no. :");
-    scanf("%d",&b);
-    printf("Enter the first no. :");
-    scanf("%d",&c);
-    int d=a+c+b;
-    printf("The sum of three numbers is : %d",d);
-    
+  
+   
 }
